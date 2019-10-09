777bae8793560e7171f4fc882c53fe9f127bb6c3
d43pan <matt@stupidventures.com>
2019-10-08 21:08:09 -0400

    added the index page

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+
+<?php
+
+?>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+<h1 class="display-4">
+Pixels2020.com
+</h1>
+
+<p class="lead">Buy pixels on 2020's hottest website.</p>
+
+
+<p> Checkout our pricing below for any number of pixels up to 2020 total.</p>
+
+<p> Want to buy <em>MORE</em> than 2020? We can work with you.  
+    Get in touch and we'd be happy to work something out
+    </p>
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">Number of Pixels</th>
+            <th scope="col">Number of Dollars</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>1</td>
+        </tr>
+        <tr>
+            <th scope="row">10</th>
+            <td>10</td>
+        </tr>
+        <tr>
+            <th scope="row">100</th>
+            <td>100</td>
+        </tr>
+        <tr>
+            <th scope="row">1000</th>
+            <td>1000</td>
+        </tr>
+        <tr>
+            <th scope="row">2020</th>
+            <td>2020</td>
+        </tr>
+        <tr>
+            <th scope="row">More?</th>
+            <td>Give us a call</td>
+        </tr>
+    </tbody>
+    
+</div>
+</div>
+
+<img src="20191008-01.png" class="img-fluid" alt="PNG version of image">
+Note - the image of record is stored in bitmap format and pixels may be inspected upon request.  We use a PNG format for the website in order to ensure the best possible experience for all of our visitors.
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
